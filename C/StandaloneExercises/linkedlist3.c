67492b796c2d3953ec7fc385fb19d10b9766fb01
TacPhoto <karol.kowalczyk.99@gmail.com>
2019-03-07 00:02:23 +0100

    ok

--- C/StandaloneExercises/linkedlist3.c
+++ C/StandaloneExercises/linkedlist3.c
@@ -1,104 +1,55 @@
 #include <stdio.h>
 #include <stdlib.h>
-#define LINE puts("*****************************")
-//Does not work yet, I do not know why, I only know there is a memory leak
-struct Node //linked list node
-{
-	int data;
-	struct Node *next;
-};
+#include "list.h"
 
-void push(struct Node** head_ref, int new_data); //insert new node as a head
-void append(struct Node** head_ref, int new_data); //append a new node to the end of the list
-void printList(struct Node *node); //prints the list
-void showMenu();
-/*************************************************/
-int main()
-{
-	char choice;
-	int dataInput;
-	struct Node* myList = NULL;  //initiate an empty list
-	
-	while(choice != '9')
-	{
-	system("cls");
-	LINE;
-	printList(myList);
-	showMenu();
-	
-	scanf("%d", choice);
-	if(choice == '9') break;
+void PrintMovie(Id id);
 
-	LINE;
-	puts("Enter number:");
-	scanf("%d", &dataInput);
-	puts(dataInput);//test
-	Sleep(1000);//delete later
-	LINE;
+int main(void)
+{
+	List movies;
+	Id temp;
+	InitList(&movies);
 	
-	switch(choice)
+	if(FullList(&movies))
 	{
-		case 1:
-			append(&myList, dataInput);
-			break;
-		case 2:
-			push(&myList, dataInput);
-			break;
+		fprintf(stderr,"Memory Full\n");
+		exit(1);
 	}
+	
+	puts("Enter first movie title:");
+	while(gets(temp.title) != NULL && temp.title != '\0') //input data to the movies list
+	{
+		puts("Enter rating <0-10>:");
+		scanf("%d", &temp.rating);
+		while(getchar() != '\n')
+			continue;
+		if(InsertEnd(temp, &movies) == false)
+		{
+			fprintf(stderr,"Memory Allocation Error\n");
+			break;
+		}
+		if(FullList(&movies))
+		{
+			puts("The list is already FULL");
+			break;
+		}
+		puts("Enter the next title:");		
 	}
-	printList(myList);
+	
+	if(EmptyList(&movies))
+	{
+		puts("No movies");
+	}
+	else
+	{
+	puts("The Movie List:");
+	ApplyToAll(&movies, PrintMovie);	
+	}
+	
 	return 0;
 }
-/*************************************************/
-void push(struct Node** head_ref, int new_data) 
-{
-	struct Node* new_node = (struct Node*) malloc(sizeof(struct Node)); //allocate a new node
-	
-	new_node->data = new_data; //assign data to node
-	
-	new_node->next = (*head_ref); //assign 'next' pointer to head
-	
-	(*head_ref) = new_node; //move head to the new node
-}
 
-void append(struct Node** head_ref, int new_data)
+void PrintMovie(Id id)
 {
-	struct Node* new_node = (struct Node*) malloc(sizeof(struct Node)); //allocate a new node
-	
-	struct Node *last = *head_ref; //it will be used later
-	
-	new_node->data = new_data; //assign data to node
-	
-	new_node->next = NULL; //assign NULL pointer to the new end of the list
-	
-	if(*head_ref == NULL) //makes sure there is only one nullptr assigned to actual end of the list
-	{
-		*head_ref = new_node;
-		return;
-	}
-	
-	while (last->next != NULL) //else traverse till the last node
-        last = last->next; 
-   
-    last->next = new_node; //Change the next of last node
-    return;     
+	printf("Movie: %s   Rating: %d\n", id.title, id.rating);
 }
-
-void printList(struct Node *node)
-{
-	while (node != NULL) //iterate untill the end of the list by recognizing nullptr at the last node
-  { 
-     printf(" %d ", node->data); //print 'data' section from the list
-     node = node->next; //set current node status to the next node
-  } 
-}
-
-void showMenu()
-{
-	LINE;
-	puts("1 to add a new node to the end of the list");
-	puts("2 to add a new node to the beginning of the list");
-	puts("9 exit");
-	LINE;	
-}
-
