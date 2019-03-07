3921ebaa9bacd5f4f84b39bb9d81596045ed4f58
TacPhoto <karol.kowalczyk.99@gmail.com>
2019-03-07 12:25:45 +0100

    added BasicIO2.py

---
+++ Python/StandaloneExercises/BasicIO2.py
@@ -0,0 +1,21 @@
+print("firts string" + " " + "second string") #concatenation
+print ("text " * 3)
+print(int("3") + int("7"))
+print(int("5" + "4"))
+
+print(  float(input("Enter a number ")) * float(input("And another one "))  )
+
+print("*****************")
+
+x = 7
+y = int(input("Enter an integer"))
+z = x * y
+print(z)
+
+print("*****************")
+
+spam = "eggs"
+print(spam * 3)
+del spam #similar to free(spam); in C :)
+
+print("*****************")
