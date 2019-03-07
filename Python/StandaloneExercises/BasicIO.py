55967bd2d7517258c225a10fa895bbe55bdc5ed3
TacPhoto <karol.kowalczyk.99@gmail.com>
2019-03-07 11:45:38 +0100

    added BasicIO.py

---
+++ Python/StandaloneExercises/BasicIO.py
@@ -0,0 +1,10 @@
+###basic input output exercise
+print(4+5)
+print(4%2)
+print(7.341230008000) #ignores additional zeroes
+print(2**3)
+print(4/2) #result is a float
+print(4//2) #result is an integer
+print("I\'t is not a hard thing yo use quotation marks in text or even\nput a new line in Python!")
+print("Enter sth:")
+input()
