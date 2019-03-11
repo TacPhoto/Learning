7cef206970cd559a56909b3ae9279d9fc3ea1c82
TacPhoto <karol.kowalczyk.99@gmail.com>
2019-03-11 14:55:14 +0100

    add

---
+++ Python/StandaloneExercises/BasicTuple.py
@@ -0,0 +1,10 @@
+tuple = ('Sth', 123, 'Something')
+print(tuple)
+print(tuple[0])
+
+first, second, third = tuple #assign tuple's info to variables
+print(second)
+print(third)
+
+oneElementTuple = (7,) #without the coma, the oneElementTuple becomes a variable, not a tuple
+print(oneElementTuple)
