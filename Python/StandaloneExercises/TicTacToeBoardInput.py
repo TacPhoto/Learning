e246e5e12f22adf64e795041574a8bb261a8a7b5
TacPhoto <karol.kowalczyk.99@gmail.com>
2019-03-14 21:23:28 +0100

    add

---
+++ Python/StandaloneExercises/TicTacToeBoardInput.py
@@ -0,0 +1,22 @@
+board = {
+    'top-l': '  ', 'top-m': '  ', 'top-r': '  ',
+    'mid-l': '  ', 'mid-m': '  ', 'mid-r': '  ',
+    'low-l': '  ', 'low-m': '  ', 'low-r': '  ',
+}
+
+def printBoard(board):
+    print(board['top-l'], '|', board['top-m'], '|', board['top-r'])
+    print('------------')
+    print(board['mid-l'], '|', board['mid-m'], '|', board['mid-r'])
+    print('------------')
+    print(board['low-l'], '|', board['low-m'], '|', board['low-r'])
+def userInput():
+    try:
+        choice = input('Select: top/mid/low - l/m/r. Example: top-r for top right cell')
+        board[choice] = ' X'
+    except:
+        print('Try again')
+
+while(True):
+    printBoard(board)
+    userInput()
