81baeb95e409cc838e6ead67fb40453d20854f11
TacPhoto <karol.kowalczyk.99@gmail.com>
2019-03-22 15:01:53 +0100

    update and correct

--- Python/MachineLearningCourseBySentdex/RegressionTraining.py
+++ Python/MachineLearningCourseBySentdex/RegressionTraining.py
@@ -7,37 +7,36 @@ from sklearn.model_selection import train_test_split
 
 ###get data
 df = quandl.get('WIKI/GOOGL') #import quandl data, df stands for 'data frame'
-#prepare downloaded data
-df = df[['Adj. Open',  'Adj. High',  'Adj. Low',  'Adj. Close', 'Adj. Volume']] #name columns
-df['HL_PCT'] = ( df['Adj. High'] - df['Adj. Low'] ) / df['Adj. Close'] * 100.00 #define HL_PCT column
-df['PCT_change'] = ( df['Adj. Close'] - df['Adj. Open'] ) / df['Adj. Open'] * 100.00 #define PCT_change column
 
-df = df[['Adj. Close', 'HL_PCT','PCT_change', 'Adj. Volume']] #define columns we need again
+#prepare downloaded data
+df = df[['Adj. Open', 'Adj. High', 'Adj. Low', 'Adj. Close',  'Adj. Volume']] #name columns
+df['HL_PCT'] = (df['Adj. High'] - df['Adj. Close']) / df['Adj. Close'] * 100 #define HL_PCT column
+df['PCT_change'] = (df['Adj. Close'] - df['Adj. Open']) / df['Adj. Open'] * 100 #define PCT_change column
+
+df = df[['Adj. Close', 'HL_PCT', 'PCT_change', 'Adj. Volume']] #define columns we need again
 
 ###set data to create forecast of
-forecast_col = 'Adj. Close' #specify forecast column (adjustable)
-df.fillna(-.9999, inplace = True) #fill missing data
+forecast_col = 'Adj. Close'
+df.fillna(-.99999, inplace=True)
+forecast_out = int(math.ceil(0.01*len(df)))
 
 ###set a timeframe to predict
-forecast_out = int(math.ceil(0.01 * len(df))) #number of days out (1%% of current data frame)
-df['label'] = df[forecast_col].shift(-forecast_out) #shift column in time
-df.dropna(inplace = True)
+df['label'] = df[forecast_col].shift(-forecast_out)
+df.dropna(inplace=True)
 
 ###create data array using numpy
-X = np.array[df.drop(['label']), 1]
+X = np.array(df.drop(['label'], 1))
 Y = np.array(df['label'])
-X = preprocessing.scale(X) #scale x before feeding to the classifier
+X = preprocessing.scale(X)
 Y = np.array(df['label'])
-
-X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size = 0.2)
 
 ###create classifier
-clf = LinearRegression(n_jobs = -1) #run as many algorithm jobs as possible paralelly
+X_train, X_test, Y_train,Y_test = train_test_split(X, Y, test_size=0.2)
+
+clf = LinearRegression()
 clf.fit(X_train, Y_train)
 
 ###check acurracy
-accuracy = clf.score(X_test, Y_test) #test clf with data he does no already know
-
-###print the result
+accuracy = clf.score(X_test, Y_test)
 print('Number of days predicted: ' + str(forecast_out))
-print('Acurracy: ' + str(accuracy)) #the result may vary every launch
+print(accuracy)
