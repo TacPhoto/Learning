03c99d2d3deffd2119400e7d2b2ba40693e3c5e3
TacPhoto <karol.kowalczyk.99@gmail.com>
2019-03-29 22:58:59 +0100

    UPDATE

--- Python/MachineLearningCourse/SVM_SVC.py
+++ Python/MachineLearningCourse/SVM_SVC.py
@@ -1,12 +1,11 @@
 import pandas as pd
 import sklearn
-from sklearn import svm
+from sklearn import svm, metrics
 from sklearn import datasets
-#########TO BE DEVELOPED ;)
-cancer = datasets.load_breast_cancer() #get data
 
-print(cancer.feature_names)
-print(cancer.target_names)
+cancer = datasets.load_breast_cancer() #get data
+#print(cancer.feature_names) #test
+#print(cancer.target_names) #test
 
 x = cancer.data #set x
 y = cancer.target #set y
@@ -15,3 +14,15 @@ x_train, x_test, y_train, y_test = sklearn.model_selection.train_test_split(x, y
 #print(x_train)
 
 classes = ['malignant', 'benign']
+
+clf = svm.SVC(kernel = "linear", C = 2) #classifier, remember: C is for the margin
+clf.fit(x_train, y_train) #feed the classifier with data
+
+y_pred = clf.predict(x_test)
+
+accuracy = metrics.accuracy_score(y_test, y_pred) #calculate accuracy
+print('Accuracy:', accuracy)
+print('---pred---')
+print(y_pred[::1])
+print('---test---')
+print(y_test[::1])
