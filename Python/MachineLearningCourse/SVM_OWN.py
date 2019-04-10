ff6548572d520915abba5eec8a065be7ddcbd030
TacPhoto <karol.kowalczyk.99@gmail.com>
2019-04-10 22:19:22 +0200

    update

--- Python/MachineLearningCourse/SVM_OWN.py
+++ Python/MachineLearningCourse/SVM_OWN.py
@@ -1,31 +1,32 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib import style
-#####NOT FULLY DEVELOPED YET!#####
+
 style.use('ggplot')
 
 class support_vector_machine:
-    def __init__(self, visualization = True):
+    def __init__(self, visualization=True):
         self.visualization = visualization
         self.colors = {1: 'r', -1: 'b'}
         if self.visualization:
             self.fig = plt.figure()
             self.ax = self.fig.add_subplot(1, 1, 1)
 
-    def fit(self, data): #train
+
+    def fit(self, data):  # train
         self.data = data
 
         opt_dict = {}
 
-        transforms = [[1,1],
-                      [-1,1],
-                      [-1,-1],
-                      [1,-1]]
+        transforms = [[1, 1],
+                      [-1, 1],
+                      [-1, -1],
+                      [1, -1]]
         all_data = []
         for yi in self.data:
             for featureset in self.data[yi]:
                 for feature in featureset:
-                    all_data.appent(feature)
+                    all_data.append(feature)
         self.max_feature_value = max(all_data)
         self.min_feature_value = min(all_data)
         all_Data = None
@@ -42,7 +43,7 @@ class support_vector_machine:
 
         for step in step_sizes:
             w = np.array([latest_optimum, latest_optimum])
-            optimized = False #for optimization purposes
+            optimized = False  # for optimization purposes
             while not optimized:
                 for b in np.arange(-1 * (self.max_feature_value * b_range_multiple),
                                    self.max_feature_value * b_range_multiple,
@@ -50,15 +51,17 @@ class support_vector_machine:
                     for transformation in transforms:
                         w_t = w * transformation
                         found_option = True
-                        for i in self.data: #weakest link in the SVM fundametally
+                        for i in self.data:  # weakest link in the SVM fundametally
                             for xi in self.data[i]:
                                 yi = i
-                                if not yi * (np.dot(w_t, xi) + b) >= 1:
+                                condit = yi * (np.dot(w_t, xi) + b)
+                                if not condit >= 1:
                                     found_option = False
                                     break
+                                #print(condit) #for notification purposes
                         if found_option:
                             opt_dict[np.linalg.norm(w_t)] = [w_t, b]
-                if w[0] < 0:
+                if w[0] < 0: #for notification purposes
                     optimized = True
                     print('Optimized a step')
                 else:
@@ -70,19 +73,49 @@ class support_vector_machine:
             self.w = opt_choice[0]
             self.b = opt_choice[1]
             latest_optimum = opt_choice[0][0] + step * 2
-            
-
 
 
     def predict(self, features):
         classification = np.sign(np.dot(np.array(features), self.w) + self.b)
 
+        if classification != 0 and self.visualization:
+            self.ax.scatter(features[0], features[1], s=  200, marker = '*', c = self.colors[classification])
+
         return classification
 
 
-data_dict = {-1:np.array([[1,7],
-                          [2,8],
-                          [3,8]]),
-             1:np.array([[5,1],
-                         [6,-1],
-                         [7,3]])}
+    def visualize(self):
+        [[self.ax.scatter(x[0], x[1], s=100, color=self.colors[i]) for x in data_dict[i]] for i in data_dict]
+
+        def hyperplane(x, w, b, v):
+            return (-w[0] * x - b + v) / w[1]
+
+        datarange = (self.min_feature_value * 0.9, self.max_feature_value * 1.1)
+        hyp_x_min = datarange[0]
+        hyp_x_max = datarange[1]
+        #positive support vector hyperplane
+        psv1 = hyperplane(hyp_x_min, self.w, self.b, 1)
+        psv2 = hyperplane(hyp_x_max, self.w, self.b, 1)
+        self.ax.plot([hyp_x_min, hyp_x_max], [psv1, psv2], 'k')
+        #negative support vector hyperplane
+        nsv1 = hyperplane(hyp_x_min, self.w, self.b, -1)
+        nsv2 = hyperplane(hyp_x_max, self.w, self.b, -1)
+        self.ax.plot([hyp_x_min, hyp_x_max], [nsv1, nsv2], 'k')
+        #positive support vector hyperplane
+        db1 = hyperplane(hyp_x_min, self.w, self.b, 0)
+        db2 = hyperplane(hyp_x_max, self.w, self.b, 0)
+        self.ax.plot([hyp_x_min, hyp_x_max], [db1, db2], 'y--')
+
+        plt.show()
+
+
+data_dict = {-1: np.array([[1, 7],
+                           [2, 8],
+                           [3, 8]]),
+             1: np.array([[5, 1],
+                          [6, -1],
+                          [7, 3]])}
+
+svm = support_vector_machine() #declare svm
+svm.fit(data = data_dict) #train svm
+svm.visualize() #plot svm
