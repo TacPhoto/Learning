986eee7cbed19bd487dfd3a9a87a5089496c6273
TacPhoto <karol.kowalczyk.99@gmail.com>
2019-11-06 21:24:43 +0100

    little cleanup

--- Java/src/exercise7.java
+++ Java/src/exercise7.java
@@ -10,7 +10,7 @@ public class exercise7
         {
             for(int j = 1; j < len; j++)
             {
-                if((j == len - i || i == j) )
+                if(j == len - i || i == j)
                     System.out.print("*");
                 else
                     System.out.print(" ");
