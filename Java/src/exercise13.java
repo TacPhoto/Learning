7fd1945c0cacc2f66a3a35fca04888b52c05736c
TacPhoto <karol.kowalczyk.99@gmail.com>
2019-11-18 21:45:37 +0100

    add

---
+++ Java/src/exercise13.java
@@ -0,0 +1,45 @@
+public class exercise13
+{
+    public static void main(String[] args)
+    {
+        System.out.println(sqr(4));
+
+    }
+
+
+    static int sqr(int a)
+    {
+        return multiply(a, a);
+    }
+
+
+    static float sqr(float a)
+    {
+        return multiply(a, a);
+    }
+
+    static double sqr(double a)
+    {
+        return multiply(a, a);
+    }
+
+
+    static int multiply(int a, int b)
+    {
+        return a * b;
+    }
+
+
+    static float multiply(float a, float b)
+    {
+        return a * b;
+    }
+
+
+    static double multiply(double a, double b)
+    {
+        return a * b;
+    }
+
+
+}
---
+++ Java/src/factorial.java
@@ -0,0 +1,16 @@
+public class factorial
+{
+    public static void main(String[] args)
+    {
+        System.out.println(factorial(4));
+    }
+
+
+    static int factorial(int n)
+    {
+        if(n == 0)
+            return 1;
+
+        return(n * factorial(n -1 ));
+    }
+}
