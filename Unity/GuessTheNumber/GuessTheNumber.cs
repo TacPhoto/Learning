127ac0a2576a08e930445099a478f988cf548e06
TacPhoto <karol.kowalczyk.99@gmail.com>
2019-12-29 17:49:48 +0100

    add

---
+++ Unity/GuessTheNumber/GuessTheNumber.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class GuessTheNumber : MonoBehaviour
+{
+    public InputField input;
+    public Text infoText;
+
+    private int guessNumber;
+    private int usersGuess;
+    
+    void Start()
+    {
+        guessNumber = Random.RandomRange(0, 100);
+    }
+
+    public void CheckGuess()
+    {
+        usersGuess = int.Parse(input.text);
+
+        if (usersGuess == guessNumber)
+            infoText.text = "You WON!";
+        else if (usersGuess > guessNumber)
+            infoText.text = "A bit too much";
+        else
+            infoText.text = "A bit more";
+
+        input.text = "";
+        
+    }
+}
