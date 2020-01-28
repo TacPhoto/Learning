5136fc1809ef1a21ee15d984969b98429f81eb49
TacPhoto <karol.kowalczyk.99@gmail.com>
2020-01-28 22:40:54 +0100

    add radio buttons

--- Java/JavaFX/src/main/MainWindowController.java
+++ Java/JavaFX/src/main/MainWindowController.java
@@ -3,13 +3,13 @@ package main;
 import javafx.application.Platform;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
 import javafx.scene.image.ImageView;
 import javafx.stage.Stage;
 
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.ResourceBundle;
 
 public class MainWindowController implements Initializable {
@@ -23,12 +23,21 @@ public class MainWindowController implements Initializable {
     @FXML
     Button button1;
     @FXML
-    TextField textField1;
+   TextField textField1;
+    @FXML
+    RadioButton radioOne, radioTwo;
+
+    static ToggleGroup tg;
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
         labelText.setText("Program is on");
-        button1.setGraphic((new ImageView("main/icon.png")));
+        button1.setGraphic(new ImageView("main/icon.png"));
+
+        List<RadioButton> rbList = new ArrayList<>();
+        rbList.add(radioOne);
+        rbList.add(radioTwo);
+        MainWindowController.prepareRadioButtons(rbList);
     }
 
     public void buttonClicked(){
@@ -36,6 +45,25 @@ public class MainWindowController implements Initializable {
         labelText.setText(txtFromTextField);
         button1.setText(txtFromTextField);
         textField1.setText("");
+    }
+
+    private static void prepareRadioButtons(List<RadioButton> rbList)
+    {
+        tg = new ToggleGroup();
+        for(RadioButton rb : rbList)
+        {
+            rb.setToggleGroup(tg);
+        }
+    }
+
+    @FXML
+    private void radioClicked()
+    {
+        RadioButton selectedRB = (RadioButton) tg.getSelectedToggle();
+        String clickedId = selectedRB.getId();
+        String clickedText = selectedRB.getText();
+
+        textField1.setText("Id: " + clickedId + " Name: " + clickedText);
     }
 
     public static void closeProgram(){
