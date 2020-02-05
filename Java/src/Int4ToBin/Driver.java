622cfbb5e6de3c26bd153c8f9a4333aff3e3c855
TacPhoto <karol.kowalczyk.99@gmail.com>
2020-02-05 19:43:30 +0100

    assserts update

--- Java/src/Int4ToBin/Driver.java
+++ Java/src/Int4ToBin/Driver.java
@@ -6,12 +6,12 @@ public class Driver
         int r = 7; int g = 5; int b = 1; int a = 6;
         Pixel pixel = new Pixel(r, g, b, a);
 
-        assert ( pixel.get_binary(r, 8).equals(pixel.get_r()) ) : "Binary a does not match get_b";
-        assert ( pixel.get_binary(g, 8).equals(pixel.get_g()) ) : "Binary a does not match get_b";
-        assert ( pixel.get_binary(b, 8).equals(pixel.get_b()) ) : "Binary a does not match get_b";
+        assert ( pixel.get_binary(r, 8).equals(pixel.get_r()) ) : "Binary r does not match get_b";
+        assert ( pixel.get_binary(g, 8).equals(pixel.get_g()) ) : "Binary g does not match get_b";
+        assert ( pixel.get_binary(b, 8).equals(pixel.get_b()) ) : "Binary b does not match get_b";
         assert ( pixel.get_binary(a, 8).equals(pixel.get_a()) ) : "Binary a does not match get_b";
         //System.out.println("Correct bin:  00000111000001010000000100000110"); //DEBUG
-        { Pixel fixed = new Pixel(7, 5, 1, 6); assert (pixel.getInt() == 117768454); }
+        { Pixel fixed = new Pixel(7, 5, 1, 6); assert (pixel.getInt() == 117768454) : "Wrong pixel value. Possible faulty dec2bin conversion"; }
 
         System.out.println(pixel.toString());
 
