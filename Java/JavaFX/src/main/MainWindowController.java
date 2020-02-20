d17885b18a747e8eb7585e8e9f2345378cfb41a2
TacPhoto <karol.kowalczyk.99@gmail.com>
2020-02-20 21:14:52 +0100

    opening new window

--- Java/JavaFX/src/main/MainWindowController.java
+++ Java/JavaFX/src/main/MainWindowController.java
@@ -6,13 +6,16 @@ import javafx.beans.value.ObservableValue;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.geometry.Insets;
+import javafx.scene.Scene;
 import javafx.scene.control.*;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
@@ -127,5 +130,16 @@ public class MainWindowController implements Initializable {
         ComboBox combo2 = new ComboBox(FXCollections.observableArrayList(weekDays));
         StackPane.setMargin(combo2, new Insets(310, 10, 10, 10));
         stackPane.getChildren().add(combo2);
+    }
+
+    public void showSubWindow1() throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader();
+        fxmlLoader.setLocation(getClass().getResource("SubWindow1.fxml"));
+        Scene scene = new Scene(fxmlLoader.load());
+        stage = new Stage();
+        stage.setTitle("Window 1");
+        stage.setResizable(false);
+        stage.setScene(scene);
+        stage.show();
     }
 }
