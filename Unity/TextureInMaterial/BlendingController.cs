1a0de2752851e399b15950a5b9e2ab5bccd18f4d
TacPhoto <karol.kowalczyk.99@gmail.com>
2020-02-29 22:11:47 +0100

    add

---
+++ Unity/TextureInMaterial/BlendingController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlendingController : MonoBehaviour
+{
+    private Material mat;
+
+    private void Start()
+    {
+        mat = GetComponent<Renderer>().material;
+    }
+
+    private void Update()
+    {
+        mat.SetFloat("_Blending", (Mathf.Sin(Time.time) * .5f + .5f));
+    }
+}
