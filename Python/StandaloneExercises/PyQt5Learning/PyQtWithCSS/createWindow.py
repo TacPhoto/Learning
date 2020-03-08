5ca094b9f093bb2e4b0372da33c6e96c72ac3261
TacPhoto <karol.kowalczyk.99@gmail.com>
2020-03-08 20:52:58 +0100

    move css to a separate string

--- Python/StandaloneExercises/PyQt5Learning/PyQtWithCSS/createWindow.py
+++ Python/StandaloneExercises/PyQt5Learning/PyQtWithCSS/createWindow.py
@@ -6,72 +6,78 @@ class MainWindow:
     def __init__(self):
         self.app = QtWidgets.QApplication(sys.argv)
         self.window = QtWidgets.QMainWindow()
-
         self.imagePath = "githubLogo.png"
+
+        self.styleSheet = """
+        QPushButton{
+        background-color: #383737;
+        color: #fefafa;
+        font-size: 15px;
+        border: 1px solid #4e4e4e;
+        }
+
+        QMainWindow{
+        background-color: #6e6e6e;
+        }
+
+        QTextEdit{
+        background-color: #f7f7f7;
+        color: #8e8e8e;
+        padding-top: 3px;
+        padding-left: 10px;
+        }
+
+        QTextEdit#passwordText{
+        background-color: #202020;
+        }
+        """
 
         self.initGui()
 
         self.window.setWindowTitle("Whatever")
         self.window.setGeometry(0, 0, 270, 410)
-        self.window.setStyleSheet("background-color: #6e6e6e") #border:3px solid #4e4e4e;
-
         self.window.show()
+        self.app.setStyleSheet(self.styleSheet)
         sys.exit(self.app.exec_())
 
     def initGui(self):
-
-        #login button
+        # login button
         self.loginButton = QtWidgets.QPushButton("Login", self.window)
         self.loginButton.setGeometry(140, 360, 120, 30)
-        self.loginButton.setStyleSheet("background-color: #383737;"
-                                       "color: #fefafa;"
-                                       "font-size: 15px;"
-                                       "border: 1px solid #4e4e4e")
+        # self.loginButton.setStyleSheet(
 
-        #register button
+        # register button
         self.registerButton = QtWidgets.QPushButton("Register", self.window)
         self.registerButton.setGeometry(10, 360, 120, 30)
-        self.registerButton.setStyleSheet("background-color: #383737;"
-                                       "color: #fefafa;"
-                                       "font-size: 15px;"
-                                       "border: 1px solid #4e4e4e")
 
-        #label that holds the image
+        # label that holds the image
         self.label = QtWidgets.QLabel(self.window)
         self.label.setGeometry(35, 20, 200, 200)
-        self.label.setStyleSheet("background-color: #6e6e6e")
         self.label.setAutoFillBackground(False)
 
-        #pseudo text field
+        # nickname text field
         self.pseudo = QtWidgets.QTextEdit(self.window)
         self.pseudo.setGeometry(25, 230, 220, 30)
-        self.pseudo.setText("Some text")
-        self.pseudo.setStyleSheet("background-color: #f7f7f7;"
-                                  "color: #8e8e8e;"
-                                  "padding-top: 3px;"
-                                  "padding-left: 10px")
+        self.pseudo.setText("Nickname")
 
-        #email text field
+        # email text field
         self.email = QtWidgets.QTextEdit(self.window)
         self.email.setGeometry(25, 270, 220, 30)
         self.email.setText("Email")
-        self.email.setStyleSheet("background-color: #f7f7f7;"
-                                  "color: #8e8e8e;"
-                                  "padding-top: 3px;"
-                                  "padding-left: 10px")
 
         # pseudo password text field
         self.password = QtWidgets.QTextEdit(self.window)
         self.password.setGeometry(25, 310, 220, 30)
         self.password.setText("Password")
-        self.password.setStyleSheet("background-color: #f7f7f7; color: #8e8e8e; padding-top: 3px; padding-left: 10px")
+        self.password.setObjectName("passwordText")
 
-        #image
+        # image
         self.image = QtGui.QImage(self.imagePath)
         self.pixmapImage = QtGui.QPixmap.fromImage(self.image)
 
-        #display image in the label
+        # display image in the label
         self.label.setPixmap(self.pixmapImage)
         self.label.setScaledContents(True)
+
 
 main = MainWindow()
