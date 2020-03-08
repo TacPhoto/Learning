08c2541cdcb310161c2c28b17e4c7a976eb0ed8c
TacPhoto <karol.kowalczyk.99@gmail.com>
2020-03-08 20:19:33 +0100

    update

--- Python/StandaloneExercises/PyQt5Learning/PyQtWithCSS/createWindow.py
+++ Python/StandaloneExercises/PyQt5Learning/PyQtWithCSS/createWindow.py
@@ -12,25 +12,59 @@ class MainWindow:
         self.initGui()
 
         self.window.setWindowTitle("Whatever")
-        self.window.setGeometry(0, 0, 270, 500)
-        self.window.setStyleSheet("border:3px solid #4e4e4e; background-color: #6e6e6e")
+        self.window.setGeometry(0, 0, 270, 410)
+        self.window.setStyleSheet("background-color: #6e6e6e") #border:3px solid #4e4e4e;
 
         self.window.show()
         sys.exit(self.app.exec_())
 
     def initGui(self):
-        self.applyButton = QtWidgets.QPushButton("Apply", self.window)
-        self.applyButton.setGeometry(140, 420, 120, 30)
-        self.applyButton.setStyleSheet("background-color: #4e4e4e; color: #f7f7f7")
 
-        self.cancelButton = QtWidgets.QPushButton("Cancel", self.window)
-        self.cancelButton.setGeometry(10, 420, 120, 30)
-        self.cancelButton.setStyleSheet("background-color: #4e4e4e; color: #f7f7f7")
+        #login button
+        self.loginButton = QtWidgets.QPushButton("Login", self.window)
+        self.loginButton.setGeometry(140, 360, 120, 30)
+        self.loginButton.setStyleSheet("background-color: #383737;"
+                                       "color: #fefafa;"
+                                       "font-size: 15px;"
+                                       "border: 1px solid #4e4e4e")
+
+        #register button
+        self.registerButton = QtWidgets.QPushButton("Register", self.window)
+        self.registerButton.setGeometry(10, 360, 120, 30)
+        self.registerButton.setStyleSheet("background-color: #383737;"
+                                       "color: #fefafa;"
+                                       "font-size: 15px;"
+                                       "border: 1px solid #4e4e4e")
 
         #label that holds the image
         self.label = QtWidgets.QLabel(self.window)
-        self.label.setGeometry(35, 50, 200, 200)
+        self.label.setGeometry(35, 20, 200, 200)
+        self.label.setStyleSheet("background-color: #6e6e6e")
         self.label.setAutoFillBackground(False)
+
+        #pseudo text field
+        self.pseudo = QtWidgets.QTextEdit(self.window)
+        self.pseudo.setGeometry(25, 230, 220, 30)
+        self.pseudo.setText("Some text")
+        self.pseudo.setStyleSheet("background-color: #f7f7f7;"
+                                  "color: #8e8e8e;"
+                                  "padding-top: 3px;"
+                                  "padding-left: 10px")
+
+        #email text field
+        self.email = QtWidgets.QTextEdit(self.window)
+        self.email.setGeometry(25, 270, 220, 30)
+        self.email.setText("Email")
+        self.email.setStyleSheet("background-color: #f7f7f7;"
+                                  "color: #8e8e8e;"
+                                  "padding-top: 3px;"
+                                  "padding-left: 10px")
+
+        # pseudo password text field
+        self.password = QtWidgets.QTextEdit(self.window)
+        self.password.setGeometry(25, 310, 220, 30)
+        self.password.setText("Password")
+        self.password.setStyleSheet("background-color: #f7f7f7; color: #8e8e8e; padding-top: 3px; padding-left: 10px")
 
         #image
         self.image = QtGui.QImage(self.imagePath)
