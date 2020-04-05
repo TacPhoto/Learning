3c41b9ff2af2d164bb929d88febd55ebd1aaa92a
TacPhoto <karol.kowalczyk.99@gmail.com>
2020-04-05 19:56:38 +0200

    add

---
+++ Unity/FirstSteps/ReplacementShader/ReplacementShader.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ReplacementShader : MonoBehaviour
+{
+    public Shader ReplacementMaterial;
+    void OnEnable()
+    {
+        if (ReplacementMaterial != null)
+            GetComponent<Camera>().SetReplacementShader(ReplacementMaterial, "RenderType");
+    }
+
+
+    void OnDisable()
+    {
+        GetComponent<Camera>().ResetReplacementShader();
+    }
+}
