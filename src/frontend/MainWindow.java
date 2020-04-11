05871bd8f7cdf65ff2aa7f88dbfebc829b45c480
TacPhoto <karol.kowalczyk.99@gmail.com>
2020-04-11 18:28:40 +0200

    Create most basic fundament of a program

---
+++ src/frontend/MainWindow.java
@@ -0,0 +1,63 @@
+package frontend;
+
+import javax.swing.*;
+import java.awt.*;
+import javax.swing.JFrame;
+
+public class MainWindow extends JFrame{
+    public MainWindow(){
+
+        //Frame
+        super( "Employee Manager" );
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setMinimumSize(new Dimension(1200, 800)); //todo: tweak min size according to tool pane size
+
+        //layout
+        init_ui();
+
+        //show window
+        setVisible(true);
+    }
+
+    private void initMenuBar(){
+        JMenuBar menuBar = new JMenuBar();
+
+        //create and add main menu items
+        JMenu fileMenu = new JMenu("File");
+        JMenu helpMenu = new JMenu("Help");
+        menuBar.add(fileMenu);
+        menuBar.add(helpMenu);
+
+        //create and add FILE menu items
+        JMenuItem menuItemOpen = new JMenuItem("Open");
+        JMenuItem menuItemSaveAs = new JMenuItem("Save as");
+
+        fileMenu.add(menuItemOpen);
+        fileMenu.add(menuItemSaveAs);
+
+        //create and add HELP menu items
+        JMenuItem menuItemAbout = new JMenuItem("About");
+
+        helpMenu.add(menuItemAbout);
+
+        //add actions to menus items
+
+        //add menuBar to mainWindow
+        getContentPane().add(BorderLayout.NORTH, menuBar);
+    }
+
+    private void init_ui(){
+
+        //initialize menu bar
+        initMenuBar();
+
+        //add main panels to the main window
+        JPanel toolsPanel = new JPanel(); //panel that contains all tools and operations
+        getContentPane().add(BorderLayout.CENTER, toolsPanel);
+
+        JTable staffTable = new JTable(); //table with staff data, editable
+        getContentPane().add(BorderLayout.SOUTH, new JScrollPane(staffTable));
+
+    }
+
+}
