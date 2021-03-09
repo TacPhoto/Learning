4029c11a46df4e8a3d583ae13636764ca7f951ed
TacPhoto <karol.kowalczyk.99@gmail.com>
2021-03-09 19:51:08 +0100

    implemented add child window button and color palette

---
+++ Cpp/sprite_editor_wx/cEditorFrame.cpp
@@ -0,0 +1,11 @@
+#include "cEditorFrame.h"
+
+cEditorFrame::cEditorFrame(wxMDIParentFrame* parent, wxString sName) : wxMDIChildFrame (parent, wxID_ANY, sName)
+{
+
+}
+
+cEditorFrame::~cEditorFrame()
+{
+
+}
--- Cpp/sprite_editor_wx/cMain.h
+++ Cpp/sprite_editor_wx/cMain.h
@@ -1,12 +1,27 @@
 #pragma once
 
 #include "wx/wx.h"
+#include "cEditorFrame.h"
 
-class cMain : public wxFrame
+class cMain : public wxMDIParentFrame
 {
 
 public:
 	cMain();
 	~cMain();
+
+private:
+	wxToolBar* m_ToolBar = nullptr;
+	wxMenuBar* m_MenuBar = nullptr;
+
+	void OnMenuNew(wxCommandEvent& evt);
+	void OnMenuOpen(wxCommandEvent& evt);
+	void OnMenuSave(wxCommandEvent& evt);
+	void OnMenuExit(wxCommandEvent& evt);
+
+	void OnSelectColour(wxCommandEvent& evt);
+
+
+	wxDECLARE_EVENT_TABLE();
 };
 
